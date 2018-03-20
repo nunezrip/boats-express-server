587c2ae1f6cfb0ecc9d711740417f6859bf2b953
nunezrip <pnunez@techlaunch.io>
2018-03-20 18:34:16 -0400

    refactor commit

--- server/server.js
+++ server/server.js
@@ -1,29 +1,48 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('lodash');
+var morgan = require('morgan');
 
 var app = express();
 
+var lions = [];
+var id = 0;
+
+var updateId = function(req, res, next) {
+	if (!req.body.id) {
+		id++;
+		req.body.id = id + '';
+	}
+	next();
+};
+
+app.use(morgan('dev'));
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var lions = [];
-var id = 0;
+app.param('id', function(req, res, next, id) {
+	var lion = _.find(lions, { id: id });
+
+	if (lion) {
+		req.lion = lion;
+		next();
+	} else {
+		res.send();
+	}
+});
 
 // routes
 app.get('/lions', (req, res) => {
 	res.json(lions);
 });
 app.get('/lions/:id', (req, res) => {
-	var lion = _.find(lions, { id: req.params.id });
+	var lion = req.lion;
 	res.json(lion || {});
 });
 
-app.post('/lions', (req, res) => {
+app.post('/lions', updateId, (req, res) => {
 	var lion = req.body;
-	id++;
-	lion.id = id + '';
 
 	lions.push(lion);
 
