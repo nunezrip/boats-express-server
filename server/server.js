3260a3bbc6a23d1fb610ab28d5e25b06b5e3e170
nunezrip <pnunez@techlaunch.io>
2018-03-21 22:18:31 -0400

    first test and started post for the client

--- server/server.js
+++ server/server.js
@@ -1,86 +1,23 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var _ = require('lodash');
 var morgan = require('morgan');
 
 var app = express();
 
-var lions = [];
-var id = 0;
-
-var updateId = function(req, res, next) {
-	if (!req.body.id) {
-		id++;
-		req.body.id = id + '';
-	}
-	next();
-};
+var lionRouter = require('./lions');
 
 // Global Middleware
 app.use(morgan('dev'));
 app.use(express.static('client'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Mount the routes
+app.use('/lions', lionRouter);
+
 app.use((err, req, res, next) => {
 	if (err) {
 		res.status(500).send(error);
 	}
 });
 
-app.param('id', function(req, res, next, id) {
-	var lion = _.find(lions, { id: id });
-
-	if (lion) {
-		req.lion = lion;
-		next();
-	} else {
-		res.send();
-	}
-});
-
-// routes
-app.get('/lions', (req, res) => {
-	res.json(lions);
-});
-app.get('/lions/:id', (req, res) => {
-	var lion = req.lion;
-	res.json(lion || {});
-});
-
-app.post('/lions', updateId, (req, res) => {
-	var lion = req.body;
-
-	lions.push(lion);
-
-	res.json(lion);
-});
-
-app.put('/lions/id:', (req, res) => {
-	var update = req.body;
-	if (update.id) {
-		delete update.id;
-	}
-
-	var lion = _.findIndex(lions, { id: req.params.id });
-	if (!lions[lion]) {
-		res.send();
-	} else {
-		var updateLion = _.assign(lion[lion], update);
-		res.json(updateLion);
-	}
-});
-
-app.delete('/lions/id:', (req, res) => {
-	var lion = _.findIndex(lions, { id: req.params.id });
-	if (!lions[lion]) {
-		res.send();
-	} else {
-		var deletedLion = lions[lion];
-		lions.splice(lion, 1);
-		res.json(deletedLion);
-	}
-});
-
-app.listen(3000, () => {
-	console.log('Listening on Port 3000');
-});
+module.exports = app;
