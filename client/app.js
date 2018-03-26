34846774153453a0332879e4963ceccd46bb1ecc
nunezrip <pnunez@techlaunch.io>
2018-03-26 18:27:50 -0400

    added some css

---
+++ client/app.js
@@ -0,0 +1,95 @@
+import { stringify } from 'querystring';
+
+function ready(fn) {
+	if (document.readyState != 'loading') {
+		fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
+}
+
+var getValues = function() {
+	var name = document.querySelector('input[name=lion-name]').value;
+	var pride = document.querySelector('input[name=lion-pride]').value;
+	var age = document.querySelector('input[type=number]').value;
+	var gender = document.querySelector('select');
+	gender = gender.options[gender.selectedIndex].value;
+
+	document.querySelector('input[name=lion-name]').value = '';
+	document.querySelector('input[name=lion-pride]').value = '';
+	document.querySelector('input[type=number]').value = '';
+
+	return {
+		name: name,
+		pride: pride,
+		age: age,
+		gender: gender,
+	};
+};
+
+var lions = [];
+
+const lionTemplate =
+	'<h3><% name %></h3>' +
+	'<h3><%= pride %></h3>' +
+	'<small> age: <%= age %></small>' +
+	'<small><%= gender %></small>';
+
+const makeTemplate = data => {
+	const li = document.createElement('li');
+	const lionList = document.querySelector('.lion-list');
+	const compiled = _.template(lionTemplate);
+	const lionHtml = compiled(data);
+	li.innerHTML = lionHtml;
+	lionList.insertBefore(li, lionList.firstChild);
+};
+
+const updateLionList = () => {
+	const lionData = lions[lions.length - 1];
+	makeTemplate(lionData);
+};
+
+const makeLionList = () => {
+	lions.forEach(lion => {
+		makeTemplate(lion);
+	});
+};
+
+const getAllLions = () => {
+	fetch('/lions')
+		.then(res => {
+			console.log(res);
+			return res.json();
+		})
+		.then(data => {
+			lions = lions.concat(data);
+			makeLionList();
+		});
+};
+
+ready(function() {
+	getAllLions();
+	var form = document.querySelector('form');
+	form.addEventListener('submit', function(e) {
+		e.preventDefault();
+
+		var values = getValues();
+
+		fetch('/lions', {
+			method: 'post',
+			headers: {
+				Accept: 'application/json',
+				'Conten-Type': 'application/json',
+			},
+			body: JSON.stringify(values),
+		})
+			.then(function(res) {
+				return res.json();
+			})
+			.then(function(createdLion) {
+				lions.push(createdLion);
+				updateLionList();
+			});
+		return false;
+	});
+});
---
+++ server/lions.js
@@ -0,0 +1,68 @@
+var lionRouter = require('express').Router();
+
+var lions = [];
+var id = 0;
+
+var updateId = function(req, res, next) {
+	if (!req.body.id) {
+		id++;
+		req.body.id = id + '';
+	}
+	next();
+};
+
+lionRouter.param('id', function(req, res, next, id) {
+	var lion = lions.find(lions => lion.id == id);
+
+	if (lion) {
+		req.lion = lion;
+		next();
+	} else {
+		res.send();
+	}
+});
+
+// routes
+lionRouter.get('/', (req, res) => {
+	res.json(lions);
+});
+lionRouter.get('/:id', (req, res) => {
+	var lion = req.lion;
+	res.json(lion || {});
+});
+
+lionRouter.post('/', updateId, (req, res) => {
+	var lion = req.body;
+
+	lions.push(lion);
+
+	res.json(lion);
+});
+
+lionRouter.put('/id:', (req, res) => {
+	var update = req.body;
+	if (update.id) {
+		delete update.id;
+	}
+
+	var lion = _.findIndex(lions, { id: req.params.id });
+	if (!lions[lion]) {
+		res.send();
+	} else {
+		var updatedLion = Object.assign({}, lion[lion], update);
+		res.json(updatedLion);
+	}
+});
+
+lionRouter.delete('/id:', (req, res) => {
+	var lion = lions.findIndex(lions => lion.id == req.params.id);
+	if (!lions[lion]) {
+		res.send();
+	} else {
+		var deletedLion = lions[lion];
+		lions.splice(lion, 1);
+		res.json(deletedLion);
+	}
+});
+
+module.exports = lionRouter;
